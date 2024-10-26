378082ded5bc3d14246678f8f5384a4722476661
sanketshirke07 <shirkesanket99@gmail.com>
2024-10-26 22:08:03 +0530

    this is First commit

---
+++ src/main/java/in/sandata/App.java
@@ -0,0 +1,13 @@
+package in.sandata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
